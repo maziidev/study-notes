37d8208fb81f8cf3e7908783341136555d03e0e3
maziidev <godswillnathaniel97@gmail.com>
2025-05-06 00:45:25 +0100

    removed some folders

--- backend/nodejs/learning-one/index.js
+++
@@ -1,9 +0,0 @@
-// console.log("Hello world");
-
-// const array = [1, 2, 3, 4];
-// console.log(array);
-
-// setTimeout(() => {
-//     console.log("Hello after 2 second");
-// }, 2000);
-
--- backend/nodejs/readme.md
+++
@@ -1,3 +0,0 @@
-# Nodejs Notes
-
-In nodejs filesystem, if your using the async way to read write or append file, you add a calback function and also check for errors
